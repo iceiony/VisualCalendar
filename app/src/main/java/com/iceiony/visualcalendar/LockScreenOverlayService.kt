ffa253dc7bc0611050c2a1e57c4d7b3f0d64e200
iceiony <iceiony@yahoo.com>
2025-06-11 11:47:40 +0100

    add service and layout for overlay on lockscreen

---
+++ app/src/main/java/com/iceiony/visualcalendar/LockScreenOverlayService.kt
@@ -0,0 +1,12 @@
+package com.iceiony.visualcalendar
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class LockScreenOverlayService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
